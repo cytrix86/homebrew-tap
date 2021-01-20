5a8c5b5f74ff271665129e10a51ab7632af53529
Fabian Kotzor <fabian@kotzor.net>
2021-01-20 12:56:57 +0300

    add formula

---
+++ global6kupdater.rb
@@ -0,0 +1,36 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Global6kupdater < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/cytrix86/global6kUpdater/archive/v0.1.0.tar.gz"
+  sha256 "c447a037cc257b2e465cfbaec3c2ea8093c07935749e0cd9efca433b5d23523a"
+  license ""
+
+  # depends_on "cmake" => :build
+
+  def install
+	bin.install "globalupdater"
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    #system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test global6kUpdater`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
