911bbcb08f6d3f66f18a0f542e9f287b6960a4f8
Fabian Kotzor <fabian@kotzor.net>
2021-01-20 14:31:44 +0300

    v0.1.1

--- global6kupdater.rb
+++ global6kupdater.rb
@@ -4,34 +4,14 @@
 class Global6kupdater < Formula
   desc ""
   homepage ""
-  url "https://github.com/cytrix86/global6kUpdater/archive/v0.1.0.tar.gz"
-  sha256 "c447a037cc257b2e465cfbaec3c2ea8093c07935749e0cd9efca433b5d23523a"
+  url "https://github.com/cytrix86/global6kUpdater/archive/v0.1.1.tar.gz"
+  sha256 "3b89f2af7880f98c518d750dbba37133425191f4a15e2bbf362432c20caf968a"
   license ""
-  verison "0.1.0""
 
-  depends_on "pv"
+  depends_on "pv" => :build
 
   def install
-    bin.install "globalupdater"
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    # Remove unrecognized options if warned by configure
-    #system "./configure", "--disable-debug",
-    #                      "--disable-dependency-tracking",
-    #                      "--disable-silent-rules",
-    #                      "--prefix=#{prefix}"
-    # system "cmake", ".", *std_cmake_args
+    bin.install "bin/globalupdater"
   end
 
-  test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test global6kUpdater`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
-  end
 end
